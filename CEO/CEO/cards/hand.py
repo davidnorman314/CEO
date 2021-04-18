6d8d1d57f9c673d681be4aee36ddcca3aa527b4a
David Norman <david@normanfamily.org>
2021-04-18 18:43:26 +0100

    feat(ceo): Add CardValue and PlayedCards classes

--- CEO/CEO/cards/hand.py
+++ CEO/CEO/cards/hand.py
@@ -1,3 +1,29 @@
+import array
+
+class CardValue:
+    def __init__(self, value:int):
+        assert value >= 0
+        assert value <= 12
+
+        self.value = value
+
+    def __eq__(self, other):
+        if isinstance(other, CardValue):
+            return self.value == other.value
+        return NotImplemented
+
+class PlayedCards:
+    def __init__(self, value : CardValue, count : int):
+        assert count >= 1
+
+        self.value = value
+        self.count = count
+
 class Hand:
+    def __init__(self):
+        self._cards = array.array('i', [0,0,0,0,0,0,0,0,0,0,0,0,0] )
+
     def add(self,x,y):
+        print (self._cards)
+        print (len(self._cards))
         return x + y
--- CEO/tests/cards/hand_test.py
+++ CEO/tests/cards/hand_test.py
@@ -5,3 +5,41 @@ def test_add():
     theHand = hand.Hand()
 
     assert theHand.add(1,2) == 3
+
+def test_CardValue():
+    cv = hand.CardValue(1)
+    assert cv.value == 1
+
+    cv = hand.CardValue(0)
+    assert cv.value == 0
+
+    cv = hand.CardValue(12)
+    assert cv.value == 12
+
+    with pytest.raises(AssertionError):
+        cv = hand.CardValue(-1)
+
+    with pytest.raises(AssertionError):
+        cv = hand.CardValue(13)
+
+    cv = hand.CardValue(1)
+    cv1 = hand.CardValue(1)
+    cv2 = hand.CardValue(2)
+
+    assert cv == cv
+    assert cv == cv1
+    assert cv != cv2
+    assert cv1 == cv
+    assert cv1 == cv1
+    assert cv1 != cv2
+    assert cv2 != cv
+    assert cv2 != cv1
+    assert cv2 == cv2
+
+def test_PlayedCards():
+    pc = hand.PlayedCards(hand.CardValue(1), 3)
+    assert pc.value == hand.CardValue(1)
+    assert pc.count == 3
+
+    with pytest.raises(AssertionError):
+        cv = hand.PlayedCards(hand.CardValue(1), 0)
