d0aaaab2b69637839fa12a6d282e663591332ffc
David Norman <david@normanfamily.org>
2021-05-13 02:55:35 +0100

    feat(ceo): Add heuristic to not split pairs

--- CEO/CEO/cards/heuristicbehavior.py
+++ CEO/CEO/cards/heuristicbehavior.py
@@ -30,20 +30,50 @@ class HeuristicBehavior(SimpleBehaviorBase, PlayerBehaviorInterface):
 
         higher_players_left = starting_position > 0 and starting_position < player_position
 
+        # Find the lowest group that can be played on a trick
+        lowest_can_play = None
+        for cvi in range(13):
+            if self._can_play_card(cvi, hand, cur_trick_value, cur_trick_count):
+                lowest_can_play = cvi
+                break
+
+        if lowest_can_play is None:
+            return None
+
         # The number of groups of cards that can't be played on the trick
-        groups_lower = sum(
-            map(lambda i: hand.count(CardValue(i)) > 0, range(cur_trick_value.value + 1))
-        )
+        groups_lower = sum(map(lambda i: hand.count(CardValue(i)) > 0, range(lowest_can_play)))
 
         # The number of groups of cards that can be played on the trick
-        groups_higher = sum(
-            map(lambda i: hand.count(CardValue(i)) > 0, range(cur_trick_value.value + 1, 13))
-        )
+        groups_higher = sum(map(lambda i: hand.count(CardValue(i)) > 0, range(lowest_can_play, 13)))
 
         if higher_players_left:
-            if groups_lower >= groups_higher:
+            if groups_lower >= groups_higher and lowest_can_play < 12:
                 return None
             else:
-                return self.play_lowest_or_pass(hand, cur_trick_value, cur_trick_count, state)
+                # return self.play_lowest_or_pass(hand, cur_trick_value, cur_trick_count, state)
+                return CardValue(lowest_can_play)
 
-        return self.play_lowest_or_pass(hand, cur_trick_value, cur_trick_count, state)
+        # return self.play_lowest_or_pass(hand, cur_trick_value, cur_trick_count, state)
+        return CardValue(lowest_can_play)
+
+    def _can_play_card(
+        self, cvi: int, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
+    ) -> bool:
+        cv = CardValue(cvi)
+        hand_count = hand.count(cv)
+
+        if hand_count == 0 or hand_count < cur_trick_count:
+            return False
+
+        if cv.value <= cur_trick_value.value:
+            return False
+
+        # Don't break up groups, with some exceptions
+        if hand_count > cur_trick_count:
+            # Do break up aces to play on singles
+            if cvi == 12 and cur_trick_count == 1:
+                return True
+
+            return False
+
+        return True
