8b700f58e9de427682cc154bed1171148cbfebd6
David Norman <david@normanfamily.org>
2021-06-24 02:08:14 +0100

    feat(ceo): Implement some actions

--- CEO/gym_ceo/envs/actions.py
+++ CEO/gym_ceo/envs/actions.py
@@ -14,3 +14,28 @@ class Actions(SimpleBehaviorBase):
             return None
 
         return playable_list[0].cv
+
+    def play_second_lowest(
+        self, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
+    ) -> CardValue:
+
+        playable_list = self.get_playable_cards(hand, cur_trick_value, cur_trick_count)
+
+        if len(playable_list) == 0:
+            return None
+
+        if len(playable_list) == 1:
+            return playable_list[0].cv
+
+        return playable_list[1].cv
+
+    def play_highest(
+        self, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
+    ) -> CardValue:
+
+        playable_list = self.get_playable_cards(hand, cur_trick_value, cur_trick_count)
+
+        if len(playable_list) == 0:
+            return None
+
+        return playable_list[-1].cv
--- CEO/tests/gym/actions_test.py
+++ CEO/tests/gym/actions_test.py
@@ -58,3 +58,112 @@ def test_play_lowest():
     assert actions.play_lowest(hand, cv6, 2) == cv7
     assert actions.play_lowest(hand, cv7, 2) == None
     assert actions.play_lowest(hand, cv8, 2) == None
+
+
+def test_play_second_lowest():
+    """
+    Test Actions.play_second_lowest
+    """
+
+    # Create CardValue objects for ease of use later
+    cv0 = CardValue(0)
+    cv1 = CardValue(1)
+    cv2 = CardValue(2)
+    cv3 = CardValue(3)
+    cv4 = CardValue(4)
+    cv5 = CardValue(5)
+    cv6 = CardValue(6)
+    cv7 = CardValue(7)
+    cv8 = CardValue(8)
+
+    # Create the object
+    actions = Actions()
+
+    # Test when a single is led
+    hand = Hand()
+    hand.add_cards(cv0, 1)
+    hand.add_cards(cv1, 1)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 1)
+
+    assert actions.play_second_lowest(hand, cv0, 1) == cv6
+    assert actions.play_second_lowest(hand, cv1, 1) == cv7
+    assert actions.play_second_lowest(hand, cv2, 1) == cv7
+    assert actions.play_second_lowest(hand, cv3, 1) == cv7
+    assert actions.play_second_lowest(hand, cv4, 1) == cv7
+    assert actions.play_second_lowest(hand, cv5, 1) == cv7
+    assert actions.play_second_lowest(hand, cv6, 1) == cv7
+    assert actions.play_second_lowest(hand, cv7, 1) == None
+    assert actions.play_second_lowest(hand, cv8, 1) == None
+
+    # Test when a pair is led
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv1, 2)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+    hand.add_cards(cv8, 2)
+
+    assert actions.play_second_lowest(hand, cv0, 2) == cv7
+    assert actions.play_second_lowest(hand, cv1, 2) == cv8
+    assert actions.play_second_lowest(hand, cv2, 2) == cv8
+    assert actions.play_second_lowest(hand, cv3, 2) == cv8
+    assert actions.play_second_lowest(hand, cv4, 2) == cv8
+    assert actions.play_second_lowest(hand, cv5, 2) == cv8
+    assert actions.play_second_lowest(hand, cv6, 2) == cv8
+    assert actions.play_second_lowest(hand, cv7, 2) == cv8
+    assert actions.play_second_lowest(hand, cv8, 2) == None
+
+
+def test_play_highest():
+    """
+    Test Actions.play_highest
+    """
+
+    # Create CardValue objects for ease of use later
+    cv0 = CardValue(0)
+    cv1 = CardValue(1)
+    cv2 = CardValue(2)
+    cv3 = CardValue(3)
+    cv4 = CardValue(4)
+    cv5 = CardValue(5)
+    cv6 = CardValue(6)
+    cv7 = CardValue(7)
+    cv8 = CardValue(8)
+
+    # Create the object
+    actions = Actions()
+
+    # Test when a single is led
+    hand = Hand()
+    hand.add_cards(cv0, 1)
+    hand.add_cards(cv1, 1)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 1)
+
+    assert actions.play_highest(hand, cv0, 1) == cv7
+    assert actions.play_highest(hand, cv1, 1) == cv7
+    assert actions.play_highest(hand, cv2, 1) == cv7
+    assert actions.play_highest(hand, cv3, 1) == cv7
+    assert actions.play_highest(hand, cv4, 1) == cv7
+    assert actions.play_highest(hand, cv5, 1) == cv7
+    assert actions.play_highest(hand, cv6, 1) == cv7
+    assert actions.play_highest(hand, cv7, 1) == None
+    assert actions.play_highest(hand, cv8, 1) == None
+
+    # Test when a pair is led
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv1, 2)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+
+    assert actions.play_highest(hand, cv0, 2) == cv7
+    assert actions.play_highest(hand, cv1, 2) == cv7
+    assert actions.play_highest(hand, cv2, 2) == cv7
+    assert actions.play_highest(hand, cv3, 2) == cv7
+    assert actions.play_highest(hand, cv4, 2) == cv7
+    assert actions.play_highest(hand, cv5, 2) == cv7
+    assert actions.play_highest(hand, cv6, 2) == cv7
+    assert actions.play_highest(hand, cv7, 2) == None
+    assert actions.play_highest(hand, cv8, 2) == None
