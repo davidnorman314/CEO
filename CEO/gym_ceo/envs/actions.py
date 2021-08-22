dadace816ba2a1654de0541f8debe83e1322a21e
David Norman <david@normanfamily.org>
2021-08-22 14:27:44 +0100

    feat(ceo): Fix play lowest actions
    Fix play lowest actions so that they don't break up sets of cards.

--- CEO/gym_ceo/envs/actions.py
+++ CEO/gym_ceo/envs/actions.py
@@ -28,9 +28,13 @@ class Actions(SimpleBehaviorBase):
 
             return self.pass_on_trick(hand, cur_trick_value, cur_trick_count)
         elif action_number == self.play_lowest_num:
-            ret = self.play_lowest(hand, cur_trick_value, cur_trick_count)
+            # ret = self.play_lowest(hand, cur_trick_value, cur_trick_count)
+            ret = self.play_lowest_without_breaking_sets(hand, cur_trick_value, cur_trick_count)
         elif action_number == self.play_second_lowest_num:
-            ret = self.play_second_lowest(hand, cur_trick_value, cur_trick_count)
+            # ret = self.play_second_lowest(hand, cur_trick_value, cur_trick_count)
+            ret = self.play_second_lowest_without_breaking_sets(
+                hand, cur_trick_value, cur_trick_count
+            )
         elif action_number == self.play_highest_num:
             ret = self.play_highest(hand, cur_trick_value, cur_trick_count)
         else:
@@ -53,6 +57,24 @@ class Actions(SimpleBehaviorBase):
 
         return playable_list[0].cv
 
+    def play_lowest_without_breaking_sets(
+        self, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
+    ) -> CardValue:
+
+        if cur_trick_value is None:
+            return self.play_lowest(hand, cur_trick_value, cur_trick_count)
+        else:
+            playable_list = self.get_playable_cards(hand, cur_trick_value, cur_trick_count)
+
+        if len(playable_list) == 0:
+            return None
+
+        for value in playable_list:
+            if hand.count(value.cv) == cur_trick_count:
+                return value.cv
+
+        return None
+
     def play_second_lowest(
         self, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
     ) -> CardValue:
@@ -69,6 +91,38 @@ class Actions(SimpleBehaviorBase):
             return playable_list[0].cv
 
         return playable_list[1].cv
+
+    def play_second_lowest_without_breaking_sets(
+        self, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
+    ) -> CardValue:
+        """
+        Play the second lowest card without breaking up any sets. If there is a single value
+        that can be played, then play it.
+        """
+
+        if cur_trick_value is None:
+            return self.play_second_lowest(hand, cur_trick_value, cur_trick_count)
+        else:
+            playable_list = self.get_playable_cards(hand, cur_trick_value, cur_trick_count)
+
+        if len(playable_list) == 0:
+            return None
+
+        found_lowest = False
+        for value in playable_list:
+            if hand.count(value.cv) == cur_trick_count:
+                if not found_lowest:
+                    found_lowest = True
+                else:
+                    return value.cv
+
+        if found_lowest:
+            # We found a value to play, but not two value to play. Play the one value.
+            for value in playable_list:
+                if hand.count(value.cv) == cur_trick_count:
+                    return value.cv
+
+        return None
 
     def play_highest(
         self, hand: Hand, cur_trick_value: CardValue, cur_trick_count: int
--- CEO/tests/gym/actions_test.py
+++ CEO/tests/gym/actions_test.py
@@ -60,6 +60,94 @@ def test_play_lowest():
     assert actions.play_lowest(hand, cv8, 2) == None
 
 
+def test_play_lowest_without_breaking_sets():
+    """
+    Test Actions.play_lowest_without_breaking_sets
+    """
+
+    # Create CardValue objects for ease of use later
+    cv0 = CardValue(0)
+    cv1 = CardValue(1)
+    cv2 = CardValue(2)
+    cv3 = CardValue(3)
+    cv4 = CardValue(4)
+    cv5 = CardValue(5)
+    cv6 = CardValue(6)
+    cv7 = CardValue(7)
+    cv8 = CardValue(8)
+
+    # Create the object
+    actions = Actions()
+
+    # Test when a single is led
+    hand = Hand()
+    hand.add_cards(cv0, 1)
+    hand.add_cards(cv1, 1)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 1)
+
+    assert actions.play_lowest_without_breaking_sets(hand, cv0, 1) == cv1
+    assert actions.play_lowest_without_breaking_sets(hand, cv1, 1) == cv6
+    assert actions.play_lowest_without_breaking_sets(hand, cv2, 1) == cv6
+    assert actions.play_lowest_without_breaking_sets(hand, cv3, 1) == cv6
+    assert actions.play_lowest_without_breaking_sets(hand, cv4, 1) == cv6
+    assert actions.play_lowest_without_breaking_sets(hand, cv5, 1) == cv6
+    assert actions.play_lowest_without_breaking_sets(hand, cv6, 1) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv7, 1) == None
+    assert actions.play_lowest_without_breaking_sets(hand, cv8, 1) == None
+
+    # Test when a pair is led
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv1, 2)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+
+    assert actions.play_lowest_without_breaking_sets(hand, cv0, 2) == cv1
+    assert actions.play_lowest_without_breaking_sets(hand, cv1, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv2, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv3, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv4, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv5, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv6, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv7, 2) == None
+    assert actions.play_lowest_without_breaking_sets(hand, cv8, 2) == None
+
+    # Test when a pair is led and there is a triple
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv1, 3)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+
+    assert actions.play_lowest_without_breaking_sets(hand, cv0, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv1, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv2, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv3, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv4, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv5, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv6, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv7, 2) == None
+    assert actions.play_lowest_without_breaking_sets(hand, cv8, 2) == None
+
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv4, 3)
+    hand.add_cards(cv5, 2)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+
+    assert actions.play_lowest_without_breaking_sets(hand, cv0, 2) == cv5
+    assert actions.play_lowest_without_breaking_sets(hand, cv1, 2) == cv5
+    assert actions.play_lowest_without_breaking_sets(hand, cv2, 2) == cv5
+    assert actions.play_lowest_without_breaking_sets(hand, cv3, 2) == cv5
+    assert actions.play_lowest_without_breaking_sets(hand, cv4, 2) == cv5
+    assert actions.play_lowest_without_breaking_sets(hand, cv5, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv6, 2) == cv7
+    assert actions.play_lowest_without_breaking_sets(hand, cv7, 2) == None
+    assert actions.play_lowest_without_breaking_sets(hand, cv8, 2) == None
+
+
 def test_play_second_lowest():
     """
     Test Actions.play_second_lowest
@@ -113,6 +201,84 @@ def test_play_second_lowest():
     assert actions.play_second_lowest(hand, cv6, 2) == cv8
     assert actions.play_second_lowest(hand, cv7, 2) == cv8
     assert actions.play_second_lowest(hand, cv8, 2) == None
+
+
+def test_play_second_lowest_without_breaking_sets():
+    """
+    Test Actions.play_second_lowest_without_breaking_sets
+    """
+
+    # Create CardValue objects for ease of use later
+    cv0 = CardValue(0)
+    cv1 = CardValue(1)
+    cv2 = CardValue(2)
+    cv3 = CardValue(3)
+    cv4 = CardValue(4)
+    cv5 = CardValue(5)
+    cv6 = CardValue(6)
+    cv7 = CardValue(7)
+    cv8 = CardValue(8)
+    cv9 = CardValue(9)
+    cv10 = CardValue(10)
+
+    # Create the object
+    actions = Actions()
+
+    # Test when a single is led
+    hand = Hand()
+    hand.add_cards(cv0, 1)
+    hand.add_cards(cv1, 1)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 1)
+
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv0, 1) == cv6
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv1, 1) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv2, 1) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv3, 1) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv4, 1) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv5, 1) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv6, 1) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv7, 1) == None
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv8, 1) == None
+
+    # Test when a pair is led
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv1, 2)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+    hand.add_cards(cv8, 2)
+
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv0, 2) == cv7
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv1, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv2, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv3, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv4, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv5, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv6, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv7, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv8, 2) == None
+
+    # Test when a pair is led and there is a triple
+    hand = Hand()
+    hand.add_cards(cv0, 2)
+    hand.add_cards(cv1, 3)
+    hand.add_cards(cv6, 1)
+    hand.add_cards(cv7, 2)
+    hand.add_cards(cv8, 2)
+    hand.add_cards(cv9, 2)
+
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv0, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv1, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv2, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv3, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv4, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv5, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv6, 2) == cv8
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv7, 2) == cv9
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv8, 2) == cv9
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv9, 2) == None
+    assert actions.play_second_lowest_without_breaking_sets(hand, cv10, 2) == None
 
 
 def test_play_highest():
