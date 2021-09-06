8ce49db8868a192c4861fdec5fa15587fbc00460
David Norman <david@normanfamily.org>
2021-09-06 15:53:41 +0100

    fix: Rearrange files

--- CEO/gym_ceo/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-# register(
-#     id="seat_ceo_env-v0",
-#     entry_point="gym_ceo.envs:SeatCEOEnv",
-# )
--- CEO/gym_ceo/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_ceo.envs.seat_ceo_env import SeatCEOEnv
