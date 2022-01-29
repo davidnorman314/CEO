87e1687cb81e05554460dec3e09661910739d863
David Norman <david@normanfamily.org>
2022-01-29 03:22:58 +0000

    fix(rl): Fix constructor for use with Azure

--- src/learning/qlearning.py
+++ src/learning/qlearning.py
@@ -25,8 +25,8 @@ class QLearning(LearningBase):
 
     _train_episodes: int
 
-    def __init__(self, env: gym.Env, base_env: gym.Env, train_episodes=100000):
-        super().__init__(env, base_env)
+    def __init__(self, env: gym.Env, base_env: gym.Env, train_episodes=100000, **kwargs):
+        super().__init__(env, base_env, **kwargs)
         self._train_episodes = train_episodes
 
     def train(self, params: dict, do_log: bool):
