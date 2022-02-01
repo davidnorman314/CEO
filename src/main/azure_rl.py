03aa4ad69f3cd48bd6dec0e48f9b02f35745710a
David Norman <david@normanfamily.org>
2022-02-01 00:25:32 +0000

    feat(rl): Show training progress
    Add code to download training progress information, convert it to
    a dataframe, and save to a pickle file.
    Add python notebook to visualize the data.

--- src/main/azure_rl.py
+++ src/main/azure_rl.py
@@ -2,8 +2,143 @@
 """
 import argparse
 import json
+import pickle
 import azure.core.exceptions
+
+import pandas as pd
+import numpy as np
+
 from azure_rl.azure_client import AzureClient
+
+
+def get_training_progress(client: AzureClient, pickle_file: str):
+    """Loads all information from each traning and creates a pickle file
+    with information about training progress for each one."""
+    trainings = client.get_all_trainings()
+
+    all_trainings = dict()
+    for training_str in trainings:
+        if len(training_str) == 0:
+            continue
+
+        training = json.loads(training_str)
+        training_id = training["training_id"]
+
+        if training["record_type"] == "start_training":
+            all_trainings[training_id] = dict()
+            all_trainings[training_id]["start_training"] = training
+        elif training["record_type"] == "end_training":
+            all_trainings[training_id]["end_training"] = training
+        elif training["record_type"] == "post_train_stats":
+            all_trainings[training_id]["post_train_stats"] = training
+        elif training["record_type"] == "train_stats":
+            if "train_stats" not in all_trainings[training_id]:
+                all_trainings[training_id]["train_stats"] = []
+
+            all_trainings[training_id]["train_stats"].append(training)
+        else:
+            print("Found other", training)
+
+    trainings_rows_list = []
+    progress_rows_list = []
+    for training_id, training_dict in all_trainings.items():
+        start_training = training_dict["start_training"]
+
+        cols = dict()
+        cols["training_id"] = training_id
+        cols["start"] = pd.to_datetime(start_training["start_time"])
+
+        if "end_training" in training_dict:
+            end_training = training_dict["end_training"]
+            cols["end"] = pd.to_datetime(end_training["stop_time"])
+            cols["finished"] = True
+        else:
+            cols["end"] = None
+            cols["finished"] = False
+
+        train_stats = None
+        if "train_stats" in training_dict:
+            train_stats = training_dict["train_stats"]
+
+        post_train_stats = None
+        if "post_train_stats" in training_dict:
+            post_train_stats = training_dict["post_train_stats"]
+            cols["final_pct_win"] = post_train_stats["pct_win"]
+        else:
+            cols["final_pct_win"] = None
+
+        trainings_rows_list.append(cols)
+
+        # Get the log messages
+        log_blob_name = start_training["log_blob_name"]
+
+        try:
+            blob = client.get_blob(log_blob_name)
+        except azure.core.exceptions.ResourceNotFoundError:
+            print(f"Blob {log_blob_name} does not exist")
+            continue
+
+        lines = blob.split("\n")
+        if len(lines[-1]) == 0:
+            lines.pop()
+
+        # Process the train stats from the log
+        all_test_stats = dict()
+
+        for line in lines:
+            line_json = json.loads(line)
+            if "record_type" not in line_json:
+                continue
+
+            if line_json["record_type"] == "test_stats":
+                all_test_stats[line_json["training_episodes"]] = line_json["pct_win"]
+
+        # Process the train stats for the training. TODO: The stats have been moved to the
+        # log file, so this can be removed.
+        for train_stats in training_dict["train_stats"]:
+            all_test_stats[train_stats["training_episodes"]] = train_stats["pct_win"]
+
+        # Process the log messages for the training.
+        for line in lines:
+            line_json = json.loads(line)
+            if "record_type" not in line_json or line_json["record_type"] == "log":
+                progress_row = dict()
+
+                episode = line_json["episode"]
+
+                progress_row["training_id"] = training_id
+                progress_row["episode"] = episode
+                progress_row["avg_rewards"] = line_json["avg_reward"]
+                progress_row["recent_rewards"] = line_json["recent_reward"]
+                progress_row["states_visited"] = line_json["states_visited"]
+                progress_row["explore_rate"] = line_json["explore_rate"]
+
+                if episode in all_test_stats:
+                    progress_row["pct_win"] = all_test_stats[episode]
+                else:
+                    progress_row["pct_win"] = None
+
+                progress_rows_list.append(progress_row)
+
+                print(training_id, episode)
+                if episode == 80000:
+                    print(training_id, line)
+
+        print("lines count", len(lines))
+        print("Progress row count", len(progress_rows_list))
+
+    trainings_df = pd.DataFrame(trainings_rows_list)
+    progress_df = pd.DataFrame(progress_rows_list)
+
+    data = dict()
+    data["trainings"] = trainings_df
+    data["progress"] = progress_df
+
+    pickeled_data = pickle.dumps(data, pickle.HIGHEST_PROTOCOL)
+
+    print("Saving results to", pickle_file)
+    with open(pickle_file, "wb") as f:
+        f.write(pickeled_data)
 
 
 def get_results(client: AzureClient):
@@ -79,6 +214,15 @@ def main():
         help="Returns the final results of all trainings.",
     )
     parser.add_argument(
+        "--get-training-progress",
+        dest="get_training_progress",
+        type=str,
+        default=None,
+        help="Downloads information about the progress over time of each training "
+        + "and saves to a pickle file. The argument is the name of the pickle file.",
+        metavar="PICKLE_FILE",
+    )
+    parser.add_argument(
         "--get-blob",
         dest="blob_name",
         type=str,
@@ -104,6 +248,8 @@ def main():
             print(training)
     elif args.get_results:
         get_results(client)
+    elif args.get_training_progress:
+        get_training_progress(client, args.get_training_progress)
     elif args.blob_name and args.filename:
         blob = client.get_blob_and_save(args.blob_name, args.filename)
     elif args.blob_name:
