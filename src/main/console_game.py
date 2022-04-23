bb3306a91134ec769e3f0882ce86f3ee6adbb45c
David Norman <david@normanfamily.org>
2022-04-23 02:40:04 +0100

    feat(ceo): Update console game
    Update to work with current CEO API.
    Add option that automatically passes cards.
    Add option to play many hands as CEO and keep statistics.

--- src/main/console_game.py
+++ src/main/console_game.py
@@ -1,7 +1,11 @@
+import argparse
+from tkinter import W
+
 import CEO.cards.game as g
 from CEO.cards.game import *
 from CEO.cards.player import *
 from CEO.cards.simplebehavior import *
+from CEO.cards.round import Round, RoundState
 
 
 class ConsoleListener(EventListenerInterface):
@@ -55,22 +59,28 @@ class ConsoleListener(EventListenerInterface):
             + ")"
         )
 
-    def lead(self, card: CardValue, count: int, index: int, player: Player):
+    def lead(
+        self,
+        cur_card_value: CardValue,
+        cur_card_count: int,
+        starting_player_index: int,
+        cur_player: Player,
+    ):
 
         self._played = [None] * len(self._players)
-        self._played[index] = card
-        self._cur_trick_size = count
+        self._played[starting_player_index] = cur_card_value
+        self._cur_trick_size = cur_card_count
 
-        plural = count > 1
+        plural = cur_card_count > 1
 
         print(
-            player.name
+            cur_player.name
             + " ("
-            + str(index)
+            + str(starting_player_index)
             + ") leads "
-            + str(count)
+            + str(cur_card_count)
             + " "
-            + card.to_display(plural)
+            + cur_card_value.to_display(plural)
         )
 
     def play_cards(self, card: CardValue, count: int, index: int, player: Player):
@@ -136,8 +146,11 @@ class ConsoleBehavior(PlayerBehaviorInterface):
 
     _listener: ConsoleListener
 
+    is_reinforcement_learning: bool
+
     def __init__(self, listener: ConsoleListener):
         self._listener = listener
+        self.is_reinforcement_learning = False
 
     def _print_hand(self, hand: Hand):
         print("Hand: ", end="")
@@ -171,16 +184,23 @@ class ConsoleBehavior(PlayerBehaviorInterface):
 
     def pass_cards(self, hand: Hand, count: int) -> list[CardValue]:
 
-        print("Pass", count, "cards")
-        self._print_hand(hand)
+        pass_automatically = True
 
-        ret = []
-        while len(ret) < count:
-            ret.append(self._get_card_to_play(hand))
+        if pass_automatically:
+            print("Passing automatically")
+            simple_behavior = SimpleBehaviorBase()
+            return simple_behavior.pass_singles(hand, count)
+        else:
+            print("Pass", count, "cards")
+            self._print_hand(hand)
+
+            ret = []
+            while len(ret) < count:
+                ret.append(self._get_card_to_play(hand))
 
         return ret
 
-    def lead(self, hand: Hand, state: RoundState) -> CardValue:
+    def lead(self, starting_player_index: int, hand: Hand, state: RoundState) -> CardValue:
 
         self._listener.print_cur_players(state)
 
@@ -191,6 +211,8 @@ class ConsoleBehavior(PlayerBehaviorInterface):
 
     def play_on_trick(
         self,
+        starting_player_index: int,
+        current_player_index: int,
         hand: Hand,
         cur_trick_value: CardValue,
         cur_trick_count: int,
@@ -214,7 +236,7 @@ class ConsoleBehavior(PlayerBehaviorInterface):
             return ret
 
 
-def main():
+def play_game():
     print("Staring game.")
 
     human_name = "David"
@@ -232,6 +254,79 @@ def main():
 
     game = g.Game(players, console_listener)
     game.play(round_count=500, do_shuffle=False)
+
+
+def play_ceo_rounds():
+    print("Playing rounds as CEO.")
+
+    human_name = "David"
+    console_listener = ConsoleListener(human_name)
+
+    human = Player(human_name, ConsoleBehavior(console_listener))
+
+    players = []
+
+    # Put the human player first
+    players.append(human)
+
+    for i in range(5):
+        name = "Basic" + str(i + 2)
+        players.append(Player(name, BasicBehavior()))
+
+    num_players = 6
+    total = 0
+    won = 0
+    while True:
+        console_listener.start_round(players)
+
+        # Deal the cards
+        deck = Deck(num_players)
+        hands = deck.deal()
+
+        # Pass cards
+        passcards = PassCards(players, hands, console_listener)
+        passcards.do_card_passing()
+
+        round = Round(players, hands, console_listener)
+        round.play()
+
+        next_round_order = round.get_next_round_order()
+
+        total += 1
+        if next_round_order[0] == 0:
+            won += 1
+
+        print(f"Won {won} of {total} or {won/total}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Do learning")
+    parser.add_argument(
+        "--play-ceo",
+        dest="play_ceo",
+        action="store_const",
+        const=True,
+        default=False,
+        help="Play many rounds as CEO.",
+    )
+    parser.add_argument(
+        "--play-game",
+        dest="play_game",
+        action="store_const",
+        const=True,
+        default=False,
+        help="Play a game.",
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    if args.play_ceo:
+        play_ceo_rounds()
+    elif args.play_game:
+        play_game()
+    else:
+        parser.usage()
 
 
 if __name__ == "__main__":
