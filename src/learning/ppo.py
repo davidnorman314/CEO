fc75fc153a5d38e3651136d0b1a37758e27fc168
David Norman <david@normanfamily.org>
2022-10-27 00:41:23 +0100

    fix(rl): Update custom policy
    Fix the custom policy class to correctly update the logits.

--- src/learning/ppo.py
+++ src/learning/ppo.py
@@ -16,6 +16,8 @@ from stable_baselines3.common.logger import HParam
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.torch_layers import MlpExtractor
+from stable_baselines3.common.type_aliases import Schedule
+from stable_baselines3.common.distributions import Distribution, CategoricalDistribution
 
 from azure_rl.azure_client import AzureClient
 
@@ -139,57 +141,124 @@ class GetInvalidActionsLayer:
         return ret
 
 
-class CustomNetwork(nn.Module):
-    """
-    Custom network for policy and value function.
-    It receives as input the features extracted by the feature extractor.
-
-    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
-    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
-    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
-    """
-
-    _mlp_extractor: MlpExtractor
-
+class CustomActorCriticPolicy(ActorCriticPolicy):
     _invalid_actions_layer: th.nn.Linear
 
     def __init__(
         self,
-        feature_dim: int,
-        observation_factory: ObservationFactory,
-        net_arch: List[Union[int, Dict[str, List[int]]]],
-        activation_fn: Type[nn.Module],
-        device: Union[th.device, str],
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Callable[[float], float],
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        activation_fn: Type[nn.Module] = nn.Tanh,
+        observation_factory: ObservationFactory = None,
+        device=None,
+        *args,
+        **kwargs,
     ):
-        super(CustomNetwork, self).__init__()
+        assert observation_factory is not None
 
-        self._mlp_extractor = MlpExtractor(feature_dim, net_arch, activation_fn)
+        self.net_arch = net_arch
+        self.observation_factory = observation_factory
 
-        self.create_invalid_actions_layer(observation_factory, device)
+        super(CustomActorCriticPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            # device=device,
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+        # Disable orthogonal initialization
+        self.ortho_init = False
 
-        # Policy network
-        # self.policy_net = th.Sequential(th.Linear(feature_dim, last_layer_dim_pi), th.ReLU())
-        # Value network
-        # self.value_net = th.Sequential(th.Linear(feature_dim, last_layer_dim_vf), th.ReLU())
+        self._create_invalid_actions_layer(observation_factory, device)
 
-        # IMPORTANT:
-        # Save output dimensions, used to create the distributions
-        self.latent_dim_pi = self._mlp_extractor.latent_dim_pi
-        self.latent_dim_vf = self._mlp_extractor.latent_dim_vf
+    def _build(self, lr_schedule: Schedule) -> None:
+        """Override the method from the base class."""
+        super(CustomActorCriticPolicy, self)._build(lr_schedule)
 
-    def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
-        return self._mlp_extractor.forward(features)
+        # Overwrite the default action_net to also adjust the probabilities based on
+        # the valid actions.
 
-    def forward_actor(self, features: th.Tensor) -> th.Tensor:
-        return self._mlp_extractor.forward_actor(features)
+    def forward(
+        self, obs: th.Tensor, deterministic: bool = False
+    ) -> Tuple[th.Tensor, th.Tensor, th.Tensor]:
+        """
+        Forward pass in all the networks (actor and critic)
 
-    def forward_critic(self, features: th.Tensor) -> th.Tensor:
-        return self._mlp_extractor.forward_critic(features)
+        :param obs: Observation
+        :param deterministic: Whether to sample or use deterministic actions
+        :return: action, value and log probability of the action
+        """
+        # Preprocess the observation if needed
+        features = self.extract_features(obs)
+        latent_pi, latent_vf = self.mlp_extractor(features)
+        # Evaluate the values for the given observations
+        values = self.value_net(latent_vf)
+        distribution = self._get_action_dist_from_latent(latent_pi, features)
+        actions = distribution.get_actions(deterministic=deterministic)
+        log_prob = distribution.log_prob(actions)
+        actions = actions.reshape((-1,) + self.action_space.shape)
+        return actions, values, log_prob
+
+    def evaluate_actions(
+        self, obs: th.Tensor, actions: th.Tensor
+    ) -> Tuple[th.Tensor, th.Tensor, th.Tensor]:
+        """
+        Evaluate actions according to the current policy,
+        given the observations.
+
+        :param obs:
+        :param actions:
+        :return: estimated value, log likelihood of taking those actions
+            and entropy of the action distribution.
+        """
+        # Preprocess the observation if needed
+        features = self.extract_features(obs)
+        latent_pi, latent_vf = self.mlp_extractor(features)
+        distribution = self._get_action_dist_from_latent(latent_pi, obs)
+        log_prob = distribution.log_prob(actions)
+        values = self.value_net(latent_vf)
+        return values, log_prob, distribution.entropy()
+
+    def get_distribution(self, obs: th.Tensor) -> Distribution:
+        """
+        Get the current policy distribution given the observations.
+
+        :param obs:
+        :return: the action distribution.
+        """
+        features = self.extract_features(obs)
+        latent_pi = self.mlp_extractor.forward_actor(features)
+        return self._get_action_dist_from_latent(latent_pi, features)
+
+    def _get_action_dist_from_latent(self, latent_pi: th.Tensor, obs: th.Tensor) -> Distribution:
+        """
+        Override the method from the base class.
+        Retrieve action distribution given the latent codes.
+
+        :param latent_pi: Latent code for the actor
+        :return: Action distribution
+        """
+        assert isinstance(self.action_dist, CategoricalDistribution)
+
+        raw_mean_actions = self.action_net(latent_pi)
+        mean_actions = self._adjust_action_logits(obs, raw_mean_actions)
+
+        # Here mean_actions are the logits before the softmax
+        return self.action_dist.proba_distribution(action_logits=mean_actions)
 
     def _adjust_action_logits(self, features: th.Tensor, raw_action_logits: th.Tensor):
         """Adjust the outputs of the policy network based on which actions are invalid."""
 
-        invalid_actions_float = self._get_invalid_actions_layer(features)
+        # print("_adjust_action_logits features", features.dtype, features)
+        # print("_adjust_action_logits raw_action_logits", raw_action_logits.dtype, raw_action_logits)
+        # print("invalid layer", self._invalid_actions_layer.dtype)
+        invalid_actions_float = self._invalid_actions_layer(features)
         invalid_actions = invalid_actions_float > 0.0
 
         # self._raw_action_logits = raw_action_logits
@@ -220,6 +289,8 @@ class CustomNetwork(nn.Module):
         if invalid_logit_value >= -10.0:
             invalid_logit_value = -10.0
 
+        # print( "raw_action_logits", raw_action_logits.shape, raw_action_logits.dtype, raw_action_logits)
+        # print("invalid_actions", invalid_actions.shape, invalid_actions.dtype, invalid_actions)
         new_action_logits = raw_action_logits.clone()
         new_action_logits[invalid_actions] = invalid_logit_value
 
@@ -239,7 +310,7 @@ class CustomNetwork(nn.Module):
 
         return new_action_logits
 
-    def create_invalid_actions_layer(self, observation_factory: ObservationFactory, device: str):
+    def _create_invalid_actions_layer(self, observation_factory: ObservationFactory, device: str):
         self._observation_factory = observation_factory
 
         (begin, end) = self._observation_factory.get_valid_action_range()
@@ -258,52 +329,24 @@ class CustomNetwork(nn.Module):
         for a in range(action_size):
             bias.append(1.0)
 
-        self._invalid_actions_layer = th.nn.Linear(feature_size, action_size, device=device)
+        self._invalid_actions_layer = th.nn.Linear(
+            feature_size,
+            action_size,
+            device=device,  # dtype=th.float32
+        )
+        if False:
+            self._invalid_actions_layer.weight = th.nn.parameter.Parameter(
+                data=th.tensor(weights, dtype=th.double, device=device).double(),
+                requires_grad=False,
+            )
+            self._invalid_actions_layer.bias = th.nn.parameter.Parameter(
+                data=th.tensor(bias, dtype=th.double, device=device).double(), requires_grad=False
+            )
         self._invalid_actions_layer.weight = th.nn.parameter.Parameter(
             data=th.tensor(weights, dtype=th.float, device=device).float(), requires_grad=False
         )
         self._invalid_actions_layer.bias = th.nn.parameter.Parameter(
             data=th.tensor(bias, dtype=th.float, device=device).float(), requires_grad=False
-        )
-
-
-class CustomActorCriticPolicy(ActorCriticPolicy):
-    def __init__(
-        self,
-        observation_space: gym.spaces.Space,
-        action_space: gym.spaces.Space,
-        lr_schedule: Callable[[float], float],
-        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
-        activation_fn: Type[nn.Module] = nn.Tanh,
-        observation_factory: ObservationFactory = None,
-        *args,
-        **kwargs,
-    ):
-        assert observation_factory is not None
-
-        self.net_arch = net_arch
-        self.observation_factory = observation_factory
-
-        super(CustomActorCriticPolicy, self).__init__(
-            observation_space,
-            action_space,
-            lr_schedule,
-            net_arch,
-            activation_fn,
-            # Pass remaining arguments to base class
-            *args,
-            **kwargs,
-        )
-        # Disable orthogonal initialization
-        self.ortho_init = False
-
-    def _build_mlp_extractor(self) -> None:
-        self.mlp_extractor = CustomNetwork(
-            self.features_dim,
-            self.observation_factory,
-            self.net_arch,
-            self.activation_fn,
-            self.device,
         )
 
 
